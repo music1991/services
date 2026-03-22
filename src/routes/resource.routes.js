4b4f7157fd0f9e47a98f12878690fe889a220c06
music1991 <sebastians201491@gmail.com>
2026-03-21 23:39:56 -0300

    add detail error

--- src/routes/resource.routes.js
+++ src/routes/resource.routes.js
@@ -7,6 +7,15 @@ router.get('/', resourceController.getResources); //no se usa aun desde front
 
 router.get('/:id', resourceController.getById);  //no se usa aun desde front
 
-router.post('/', upload.single('file'), resourceController.uploadResource);
+router.post('/', (req, res, next) => {
+  upload.single('file')(req, res, (err) => {
+    if (err) {
+      // ESTE LOG TE DIRÁ EL ERROR REAL (Ej: "File too large" o "Invalid API Key")
+      console.error("❌ ERROR EN LA SUBIDA:", err.message);
+      return res.status(500).json({ error: err.message });
+    }
+    next(); // Si no hay error, va a tu controlador
+  });
+}, resourceController.uploadResource);
 
 module.exports = router;
