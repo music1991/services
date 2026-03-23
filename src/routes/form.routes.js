ce735dd5b19986fe1c6429d00d4f12643c9e7c2d
music1991 <sebastians201491@gmail.com>
2026-03-23 18:09:13 -0300

    add new flow

--- src/routes/form.routes.js
+++ src/routes/form.routes.js
@@ -4,4 +4,8 @@ const formController = require('../controllers/form.controller');
 
 router.post('/', formController.handleGoogleFormsWebhook);
 
+router.get('/', (req, res) => {
+  res.json({ message: "El endpoint de formularios está VIVO", path: "/api/form/test" });
+});
+
 module.exports = router; 
